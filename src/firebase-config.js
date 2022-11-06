b2a3b0007007141cf58da6e4e33b5adbf0be51f5
Akash N <x.jey.vii@gmail.com>
2022-11-06 23:05:19 +0530

    project finished

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,13 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 import {getFirestore} from "@firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCz9G1EOp5cl9q_zyeB2bhISkZj2OZw0iM",
+    apiKey: "Type your api key",
     authDomain: "simple-fb-app.firebaseapp.com",
     projectId: "simple-fb-app",
     storageBucket: "simple-fb-app.appspot.com",
-    messagingSenderId: "1040234307456",
-    appId: "1:1040234307456:web:83f5ea0d7d756660706b65",
-    measurementId: "G-HRY69W9S8P"
+    messagingSenderId: "Type your sender id",
+    appId: "Type your app id",
+    measurementId: "Type your measurement id"
   };
 
 const app = initializeApp(firebaseConfig);
